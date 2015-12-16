393b638fe414046cf6c9c142ba51b475d4c4d127
Giovanni Santiago 4035a20 <giovanni.santiago@upr.edu>
2015-12-16 00:29:38 -0400

    Added basic cursor tracking

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,34 @@
 $(document).ready(function() {
-	var canvas = $('#maincanvas')[0],
+	var $canvas = $('#maincanvas'),
+		canvas = $canvas[0],
 		context = canvas.getContext('2d'),
 		width = canvas.width,
 		height = canvas.height;
-	context.fillStyle = "white";
-	context.fillRect(0,0,width,height);
-	context.fillStyle = "black";
-	context.font = "40px Georgia";
-	context.textAlign = "center";
-	context.fillText("HAI",width/2,height/2);
+	
+	var cursorPos = { x:0, y:0 },
+		cursorSize = 30;
+	
+	$canvas.on("mousemove", function(e) {
+		var curpos = Utils.mousePos(canvas,e);
+		cursorPos.x = curpos.x;
+		cursorPos.y = curpos.y;
+	})
+	
+	update();
+	
+	function update() {
+		context.fillStyle = "white";
+		context.fillRect(0,0,width,height);
+		
+		context.strokeStyle = "gray";
+		context.beginPath();
+		context.moveTo(cursorPos.x - cursorSize/2, cursorPos.y);
+		context.lineTo(cursorPos.x + cursorSize/2, cursorPos.y);
+		
+		context.moveTo(cursorPos.x, cursorPos.y - cursorSize/2);
+		context.lineTo(cursorPos.x, cursorPos.y + cursorSize/2);
+		context.stroke();
+		
+		requestAnimationFrame(update);
+	}
 });
